5f79f9a271925def6d357a86cd5c4a97f1fb7dd9
Murod <parmonov98@yandex.ru>
2020-11-24 07:57:07 +0500

    Updates

---
+++ precache-manifest.d6e390bc6f1ad4190f020fc62c222284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164674d9c5f65fd43f20afab433298c3",
+    "url": "/cm/index.html"
+  },
+  {
+    "revision": "c764d1858d3e677c84a6",
+    "url": "/cm/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "83e8a81e69bf7fc5a179",
+    "url": "/cm/static/css/main.7439af7d.chunk.css"
+  },
+  {
+    "revision": "c764d1858d3e677c84a6",
+    "url": "/cm/static/js/2.10e23891.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cm/static/js/2.10e23891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e8a81e69bf7fc5a179",
+    "url": "/cm/static/js/main.85e23b1d.chunk.js"
+  },
+  {
+    "revision": "cacf63ed1bed3abf7084",
+    "url": "/cm/static/js/runtime-main.c01bb304.js"
+  }
+]);
